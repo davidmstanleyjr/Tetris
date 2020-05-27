63e142ffb9d6203a90aaa525a5c9fd1f984ceb6f
David Stanley <davidmstanleyjr@gmail.com>
2020-05-26 21:59:00 -0400

    more tetromino logic

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 	//grid stuff
 	const grid = document.querySelector('.grid');
+	const displaySquares = document.querySelector('.previous-grid div');
 	let squares = Array.from(grid.querySelectorAll('div'));
 	const width = 10;
 	const height = 20;
 	let currentPosition = 4;
+
+	//this assigns functions to keycodes
+	function control(e) {
+		if (e.KeyCode === 39) {
+			moveRight();
+		} else if (e.KeyCode === 38) {
+			rotate();
+		} else if (e.KeyCode === 37) {
+			moveLeft();
+		} else if (e.KeyCode === 40) {
+			moveDown();
+		}
+	}
+	document.addEventListener('keyup', control);
 
 	//Tetrominoes
 	const lTetromino = [
@@ -77,4 +92,39 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 		draw();
 	}
+
+	function moveLeft() {
+		undraw();
+		const isAtLeftedge = current.some((index) => (currentPosition + index) % width === 0);
+		if (!isAtLeftedge) currentPosition -= 1;
+		if (current.some((index) => squares[currentPosition + index].classList.contains('block2')))
+			currentPosition += 1;
+	}
+	draw();
+
+	//rotate Tetromino
+
+	function rotate() {
+		undraw();
+		currentRotation++;
+		if (currentRotation === current.length) {
+			currentRotation = 0;
+		}
+		current = theTetrominoes[random][currentRotation];
+		draw();
+	}
+
+	//show previous Tetromino
+	const displayWidth = 4;
+	const displayIndex = 0;
+
+	const smallTetrominoes = [
+		[ 1, displayWidth + 1, displayWidth * 2 + 1, 2 ],
+		[ 0, displayWidth, displayWidth + 1, displayWidth * 2 + 1 ],
+		[ 1, displayWidth, displayWidth + 1, displayWidth + 2 ],
+		[ 0, 1, displayWidth, displayWidth + 1 ],
+		[ 1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1 ]
+	];
+
+	// draw();
 });
